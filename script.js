0f14131bf3b73ddd3f198badb8bd25c577bebcff
pepsicolasingas <140737588+pepsicolasingas@users.noreply.github.com>
2023-09-20 21:56:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const body = document.body;
+const constBotonModo = document.getElementById("BotonModo");
+
+// Aqui ocurre la magia
+constBotonModo.addEventListener("click", () => {body.classList.toggle("dark-mode");
+
+// Verifica si el modo oscuro está activado
+const isDarkMode = body.classList.contains("dark-mode");
+  
+// Actualiza el texto del botón y el emoji en función del modo
+if (isDarkMode) {constBotonModo.textContent = "🌞";} else {constBotonModo.textContent = "🌙";}});
+
